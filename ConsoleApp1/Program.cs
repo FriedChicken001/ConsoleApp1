04b21b6919dc3369ac795e0ae2d3dac0e1cab43b
Jessica Major <jessica.g.major@gmail.com>
2024-02-12 18:32:49 -0700

    learning how to use c# because I am poor

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int h = 7;
+
+            string k = "pepperoni";
+
+            Console.WriteLine("I hate this stuff" + h + k);
+
+           
+
+            bool pepperoni = true;
+
+            double triple = 1.5;
+
+            // string interporlation: incorporates my values into the string  // string cacatonation: impplementing the  values within the string
+
+        }
+    }
+}
